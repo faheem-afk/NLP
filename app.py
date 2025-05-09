22bd57c7519cbaf69248342eaf9da02f9863ead2
Faheem <132340055+AhmAdO9@users.noreply.github.com>
2025-05-09 10:41:46 +0100

    Update app.py
    Added logging

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from Prediction import Prediction
-from data_ingestion import DataIngestion
+import json
+from datetime import datetime
+import os
 
 app = Flask(__name__)
 CORS(app) 
+
+def log_interaction(user_input, model_name, prediction):
+    log_entry = {
+        "timestamp": datetime.utcnow().isoformat(),
+        "input": user_input,
+        "model": model_name,
+        "prediction": prediction
+    }
+
+    os.makedirs('logs', exist_ok=True)
+
+    with open("logs/logs.jsonl", "a") as f:
+        f.write(json.dumps(log_entry) + "\n")
 
 @app.route('/')
 def home():
@@ -21,10 +36,13 @@ def predict():
 
     if model == 'rnn':
         message = pred.prediction()
+        log_interaction((user_input_1, user_input_2), model, message)
     elif model == 'lstm':
        message = pred.prediction()
+       log_interaction((user_input_1, user_input_2), model, message)
     elif model == 'gru':
         message = pred.prediction() 
+        log_interaction((user_input_1, user_input_2), model, message)
     else:
         message = "Invalid model selected."
 
