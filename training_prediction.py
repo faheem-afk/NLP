a42a6be92a43a144dba2fdb984f0c2d26539184f
Faheem <132340055+AhmAdO9@users.noreply.github.com>
2025-05-10 02:37:07 +0100

    Update training_prediction.py

--- training_prediction.py
+++ training_prediction.py
@@ -1,4 +1,4 @@
-from utils import find_unique_labels
+from utils import find_unique_labels, logger, load_data
 import settings
 import torch.nn as nn
 import torch
@@ -10,43 +10,56 @@ from modelling import Model_LSTM, Model_RNN, Model_GRU
 
 
 class TrainingPrediction():
-    def __init__(self, dataloaderObject, lstmObject, BertModelObject, final_embeddings_test, DataPreprocessingObject, model_name):
+   
+    def __init__(self, train_loaderObject, validation_loaderObject, lstmObject, BertModelObject, final_embeddings_test, final_embeddings_validation, model_name):
         self.tokenized_dataset_test = BertModelObject.tokenized_dataset_test
+        self.tokenized_dataset_validation = BertModelObject.tokenized_dataset_validation
         self.device = settings.deviceOption()
         self.epochs = settings.epochs
         self.lr = settings.lr
         self.final_embeddings_test = final_embeddings_test
-        self.df_train, self.df_test = DataPreprocessingObject.pre_processing()
+        self.final_embeddings_validation = final_embeddings_validation
+        self.df_train, _ , _ = load_data("data/preprocessed_data")
         self.model = lstmObject
-        self.loader = dataloaderObject
+        self.train_loader = train_loaderObject
+        self.validation_loader = validation_loaderObject
         self.model_name = model_name
 
-
+  
     def Training_and_prediction(self):
         model = self.model.to(self.device)
-        criterion = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=self.lr)
+        self.optimizer = torch.optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.CrossEntropyLoss()
+        
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+    self.optimizer, mode='min', factor=0.5, patience=3, verbose=True
+)
 
         for epoch in range(self.epochs):
             total_loss = 0
-            
-            for batch_inputs, batch_pos, batch_labels in self.loader:
+            model.train()
+            for batch_inputs, batch_pos, batch_labels in self.train_loader: 
 
                 batch_inputs, batch_pos, batch_labels = batch_inputs.to(self.device), batch_pos.to(self.device), batch_labels.to(self.device)
         
-                optimizer.zero_grad()
+                self.optimizer.zero_grad()
         
                 y_pred = model(batch_inputs, batch_pos).view(-1, 4)
 
-                loss = criterion(y_pred, batch_labels.view(-1))
+                loss = self.criterion(y_pred, batch_labels.view(-1))
         
                 loss.backward()
         
-                optimizer.step()
+                self.optimizer.step()
         
-                total_loss += loss
+                total_loss += loss.item()
                 
-            print(total_loss)
+            print(total_loss / len(self.train_loader))
+            
+            vald_loss = self.compute_validation(model)
+
+            scheduler.step(vald_loss)
+
         print(f"{'='*80}Training Complete using{model}{'=' * 80}")
 
         model.to('cpu')
@@ -61,30 +74,50 @@ class TrainingPrediction():
             model = Model_GRU().to(self.device)
 
         model.load_state_dict(torch.load(f"artifacts/{self.model_name}.pt", map_location=torch.device(self.device)))
-
-        model.eval()
        
         self.report(model, self.tokenized_dataset_test)
        
         print(f"{'='*80}Prediction Complete using{self.model_name}{'=' * 80}")
 
+   
+    def compute_validation(self, model):
+        model.eval()
+        
+        with torch.no_grad():
+            total_loss_validation = 0
+
+            for batch_inputs, batch_pos, batch_labels in self.validation_loader:
+
+                batch_inputs, batch_pos, batch_labels = batch_inputs.to(self.device), batch_pos.to(self.device), batch_labels.to(self.device)
+        
+                y_pred = model(batch_inputs, batch_pos).view(-1, 4)
+
+                loss = self.criterion(y_pred, batch_labels.view(-1))            
+        
+                total_loss_validation += loss.item()
+            
+            avg_loss = total_loss_validation  / len(self.validation_loader)
+        
+            return avg_loss
+
 
     def make_pred(self, model, df):
-        labels = [i for i in self.df_test['ner_tags']]
-        
+
         with torch.no_grad():
             output = model(self.final_embeddings_test.to(self.device), torch.tensor(df['pos']).to(self.device)).view(-1, 4)
         
             probs = torch.nn.functional.softmax(output, dim=1)
             value, index = torch.max(probs, dim=1)
 
-        return index, torch.tensor(df['labels']), labels
+        return index, torch.tensor(df['labels'])
 
 
+   
     def report(self, model, df):
-        prediction, padded_true_labels, true_labels = self.make_pred(model, df)
-
+        model.eval()
         
+        prediction, padded_true_labels = self.make_pred(model, df)
+
         prediction = prediction.cpu().numpy()
         
         padded_true_labels = padded_true_labels.cpu().numpy()
@@ -96,7 +129,6 @@ class TrainingPrediction():
         for ix, i in enumerate(padded_true_labels):
             pred_inner_sent = []
             actual_ = []
-            names_inner = []
             for jx, j in enumerate(i):
                 if j == -100:
                     pass
@@ -132,7 +164,16 @@ class TrainingPrediction():
             except KeyError:
                 continue
 
-        
+        res = {
+
+            "B-AC": np.array(output_0).mean(),
+            "B-LF": np.array(output_1).mean(),
+            "I-LF": np.array(output_2).mean(),
+            "O": np.array(output_3).mean(),
+        }
+
+        logger(res, self.model_name)
+
         print(f"B-AC: {np.array(output_0).mean()}")
         print(f"B-LF: {np.array(output_1).mean()}")
         print(f"I-LF: {np.array(output_2).mean()}")
@@ -151,3 +192,5 @@ class TrainingPrediction():
         plt.ylabel('True Labels')
         plt.title('Confusion Matrix')
         plt.show()
+
+        return res
