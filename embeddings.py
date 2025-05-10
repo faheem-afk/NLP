b0687219ed29ed2cee53abc9b529c916ec96b2df
Faheem <132340055+AhmAdO9@users.noreply.github.com>
2025-05-10 02:35:44 +0100

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader, TensorDataset
 class GenerateEmbeddings():
     def __init__(self, BertModelTrainingObject):
         self.Bert = BertModelTrainingObject
-        self.model = BertModelTrainingObject.bert_model()
+        self.model = self.Bert.bert_model()
         self.device = settings.deviceOption()
     
 
@@ -17,57 +17,52 @@ class GenerateEmbeddings():
         input_ids = torch.tensor(self.Bert.tokenized_dataset_train['input_ids'])
         attention_mask = torch.tensor(self.Bert.tokenized_dataset_train['attention_mask'])
         token_type_ids = torch.tensor(self.Bert.tokenized_dataset_train['token_type_ids'])
-
         dataset_train = TensorDataset(input_ids, attention_mask, token_type_ids)
         dataloader_train = DataLoader(dataset_train, batch_size=16)  
         
         input_ids = torch.tensor(self.Bert.tokenized_dataset_test['input_ids'])
         attention_mask = torch.tensor(self.Bert.tokenized_dataset_test['attention_mask'])
         token_type_ids = torch.tensor(self.Bert.tokenized_dataset_test['token_type_ids'])
-
         dataset_test = TensorDataset(input_ids, attention_mask, token_type_ids)
         dataloader_test = DataLoader(dataset_test, batch_size=16)  
+       
+        input_ids = torch.tensor(self.Bert.tokenized_dataset_validation['input_ids'])
+        attention_mask = torch.tensor(self.Bert.tokenized_dataset_validation['attention_mask'])
+        token_type_ids = torch.tensor(self.Bert.tokenized_dataset_validation['token_type_ids'])
+        dataset_validation = TensorDataset(input_ids, attention_mask, token_type_ids)
+        dataloader_validation = DataLoader(dataset_validation, batch_size=16)  
 
         self.model.eval()
         self.model.to(self.device)
 
-        all_embeddings_train = []
-        all_embeddings_test = []
-
         with torch.no_grad():
-            for batch_train, batch_test in zip(dataloader_train, dataloader_test):
-                
-                b_input_ids_train, b_attn_mask_train, b_token_type_ids_train = [x.to(self.device) for x in batch_train]
-                b_input_ids_test, b_attn_mask_test, b_token_type_ids_test = [x.to(self.device) for x in batch_test]
+            all_embeddings_train = []
+            for batch in dataloader_train:
+                b_input_ids, b_attn_mask, b_token_type_ids = [x.to(self.device) for x in batch]
+                outputs = self.model(input_ids=b_input_ids, attention_mask=b_attn_mask, token_type_ids=b_token_type_ids, output_hidden_states=True)
+                embeddings = torch.mean(torch.stack(outputs.hidden_states[-4:]), dim=0)
+                all_embeddings_train.append(embeddings)
 
-                outputs_train = self.model(
-                    input_ids=b_input_ids_train,
-                    attention_mask=b_attn_mask_train,
-                    token_type_ids=b_token_type_ids_train,
-                    output_hidden_states=True
-                )
+            all_embeddings_test = []
+            for batch in dataloader_test:
+                b_input_ids, b_attn_mask, b_token_type_ids = [x.to(self.device) for x in batch]
+                outputs = self.model(input_ids=b_input_ids, attention_mask=b_attn_mask, token_type_ids=b_token_type_ids, output_hidden_states=True)
+                embeddings = torch.mean(torch.stack(outputs.hidden_states[-4:]), dim=0)
+                all_embeddings_test.append(embeddings)
 
-                outputs_test = self.model(
-                    input_ids=b_input_ids_test,
-                    attention_mask=b_attn_mask_test,
-                    token_type_ids=b_token_type_ids_test,
-                    output_hidden_states=True
-                )
-
-                all_hidden_states_train = outputs_train.hidden_states
-                all_hidden_states_test = outputs_test.hidden_states
-                
-                embeddings_train = torch.mean(torch.stack(all_hidden_states_train[-4:]), dim=0)
-                embeddings_test = torch.mean(torch.stack(all_hidden_states_test[-4:]), dim=0)
-
-                all_embeddings_train.append(embeddings_train)
-                all_embeddings_test.append(embeddings_test)
+            all_embeddings_validation = []
+            for batch in dataloader_validation:
+                b_input_ids, b_attn_mask, b_token_type_ids = [x.to(self.device) for x in batch]
+                outputs = self.model(input_ids=b_input_ids, attention_mask=b_attn_mask, token_type_ids=b_token_type_ids, output_hidden_states=True)
+                embeddings = torch.mean(torch.stack(outputs.hidden_states[-4:]), dim=0)
+                all_embeddings_validation.append(embeddings)
 
         
         final_embeddings_train = torch.cat(all_embeddings_train, dim=0)
         final_embeddings_test = torch.cat(all_embeddings_test, dim=0)
-        
-        return final_embeddings_train, final_embeddings_test
+        final_embeddings_validation = torch.cat(all_embeddings_validation, dim=0)
+
+        return final_embeddings_train, final_embeddings_test, final_embeddings_validation
 
 
 
